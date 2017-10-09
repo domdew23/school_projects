62dfc9808a103887a8b9767ed3a6a2430e50b202
domdew23 <ddewhurs@oswego.edu>
2017-10-09 09:31:04 -0400

    update

--- system_simulations/network_model/main.java
+++ system_simulations/network_model/main.java
@@ -9,6 +9,19 @@ public class main {
 		// output of XOR2 is combined with output of the network
 		// output of XOR2 is also combined with input to a memory atomic model, M
 		// output of M is combined with input to XOR2
+		// output and state values of each XOR model is a single bit
+		// input set of each XOR models contains two bits {b1, b2}
+		// output function of each XOR model is lamda(s) = {s}
+		// state transition function of each XOR model is delta(s, {b1, b2}) = b1 XOR b2
+		// input and output of M are a single bit
+		// state of M is a pair of bits (q1, q2)
+		// state transition function of M is delta((q1, q2), {x}) = (q2, x)
+
+		// Clients should be able to:
+		// 1) control rate at which time in the simulation advances
+		// 2) be notified when any component, atomic or network, produces output and when an atomic component changes state
+		// 3) inject input into a running simulation to support interactive simulations
+		
 
 		if (true ^ false){
 			System.out.println("yes");
