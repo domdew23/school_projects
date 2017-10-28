d9e3c903c10cc24cc04463c1bc3df37e6ec93c2b
domdew23 <ddewhurs@oswego.edu>
2017-10-28 17:55:09 -0400

    update

---
+++ system_simulations/network_model/Network.java
@@ -0,0 +1,77 @@
+/* Composite */
+public class Network implements AtomicModel{
+	private AtomicModel[] components;
+	private boolean state;
+	private int componentCount;
+	private int tick;
+
+	private Network(){
+	}
+
+	public static NetworkBuilder builder(){
+		return new NetworkBuilder();
+	}
+
+	public boolean lambda(){
+		return state;
+	}
+
+	public void delta(boolean[] X){
+		for (int i = 0; i < componentCount; i++){
+			boolean[][] inputs = coupling(X);
+			for (int j = 0; j < componentCount; j++){
+				components[j].delta(inputs[j]); // input to the Network
+			}
+			System.out.println();
+		}
+		// network does not have state, it's state is the state of all of its components
+		// output of the network (in this case) is output of XOR2 - output is not the state
+		state = components[1].lambda();
+		System.out.println("\nNetwork state: " + ((state) ? 1 : 0) + "\n");
+		tick++;
+	}
+
+	// need to make coupling function more abstract
+	// **should not be hard coded in **
+	private boolean[][] coupling(boolean[] X){
+		boolean XOR1y = components[0].lambda(); // coupled with input to XOR2
+		boolean XOR2y = components[1].lambda(); // coupled with output of network AND input to M
+		boolean My = components[2].lambda(); // coupled with input to XOR2
+
+		boolean[] XOR1x = X;
+		boolean[] XOR2x = {XOR1y, My};
+		boolean[] Mx = {My, XOR2y};
+		boolean[][] retValue = {XOR1x, XOR2x, Mx};
+
+		return retValue;
+	}
+
+	public int getTick(){
+		return this.tick;
+	}
+
+	// coupling function push over to dest
+
+	public static class NetworkBuilder {
+		private AtomicModel[] components = new AtomicModel[3];
+		private int componentCount = 0;
+
+		public NetworkBuilder(){
+		}
+
+		public NetworkBuilder addComponent(AtomicModel component){
+			this.components[componentCount] = component;
+			this.componentCount++;
+			return this;
+		}
+
+		public Network build(){
+			Network network = new Network();
+			network.components = this.components;
+			network.componentCount = this.components.length;
+			network.state = false;
+			network.tick = 0;
+			return network;
+		}
+	}
+}
