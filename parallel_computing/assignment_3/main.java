4c150c6da7f9f87b168ef96cd6ba4ad4d0464b7f
domdew23 <ddewhurs@oswego.edu>
2017-12-11 23:01:28 -0500

    creating sockets and serversockets

--- parallel_computing/assignment_3/main.java
+++ parallel_computing/assignment_3/main.java
@@ -1,6 +1,18 @@
 public class main{
 	public static void main(String[] args){
-		Settings settings;
+		
+		Settings settings = null;
+		init(settings, args);
+
+		Control control = new Control();
+		GraphicsEngine graphicsEngine = new GraphicsEngine(control);
+		Jacobi jacobi = new Jacobi(control.A, control.B, 0, Settings.HEIGHT, 0, Settings.WIDTH, Settings.MAX_STEPS, Settings.THRESHOLD);
+		graphicsEngine.start();
+		jacobi.invoke();
+		//run(jacobi);
+	}
+
+	private static void init(Settings settings, String[] args){
 		if (args.length < 1){
 			settings = new Settings();
 		} else if (args.length == 1){
@@ -9,12 +21,6 @@ public class main{
 			System.out.println("Unexpected amount of arguments.");
 			return;
 		}
-		Control control = new Control();
-		GraphicsEngine graphicsEngine = new GraphicsEngine(control);
-		Jacobi jacobi = new Jacobi(control.A, control.B, 0, Settings.HEIGHT, 0, Settings.WIDTH, Settings.MAX_STEPS, Settings.THRESHOLD);
-		graphicsEngine.start();
-		jacobi.invoke();
-		//run(jacobi);
 	}
 
 	private static void run(Jacobi jacobi){
